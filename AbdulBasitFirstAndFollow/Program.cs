051198c85333311f490d7fc0c05f6aaf8d9cab5a
Abdul Basit <basitbhutto333@gmail.com>
2021-09-11 01:06:00 +0500

    first commit

---
+++ AbdulBasitFirstAndFollow/Program.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace MyLL1
+{
+    public interface IRule
+    {
+        string Name { get; }
+    }
+
+    public class Token : IRule
+    {
+        public string Name { get; }
+
+        public Token(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static Token Eof = new Token("$");
+        public static Token Empty = new Token("ε");
+    }
+
+    public class Rule : IRule
+    {
+        public string Name { get; }
+
+        public IEnumerable<IEnumerable<IRule>> Definitions { get; }
+
+        public Rule(string name, IEnumerable<IEnumerable<IRule>> definitions)
+        {
+            Name = name;
+            Definitions = definitions;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}->" + string.Join("/", Definitions.Select(definition =>
+                string.Join("", definition.Select(rule => rule.Name))
+                ));
+        }
+    }
+
+    public class RuleManager
+    {
+        public List<Rule> Rules { get; } = new List<Rule>();
+
+        private Dictionary<string, Token> TokenMap = new Dictionary<string, Token>();
+
+        private Token GetOrCreateToken(string tokenName)
+        {
+            if (tokenName == "$")
+                return Token.Eof;
+            if (tokenName == "ε")
+                return Token.Empty;
+            if (!TokenMap.ContainsKey(tokenName))
+                TokenMap.Add(tokenName, new Token(tokenName));
+            return TokenMap[tokenName];
+        }
+
+        public Rule CreateTextRule(string text)
+        {
+            var segments = text.Split(new[] { "->" }, StringSplitOptions.None);
+            var name = segments[0];
+            var ruleText = segments[1];
+
+            var ruleDefinitions = ruleText
+                .Split('/')
+                .Select(ruleSequenceText =>
+                {
+                    return ruleSequenceText.Select(token =>
+                    {
+                        return char.IsUpper(token) ?
+                            Rules.First(x => x.Name == token.ToString()) :
+                            (IRule)GetOrCreateToken(token.ToString());
+                    });
+                });
+            return new Rule(name, ruleDefinitions);
+        }
+
+        public IEnumerable<Token> First(IEnumerable<IEnumerable<IRule>> definitions)
+        {
+            return definitions.SelectMany(ruleSequence =>
+            {
+                var tokens = new List<Token>();
+                var hasEmpty = true;
+                foreach (var item in ruleSequence)
+                {
+                    if (item is Token token)
+                    {
+                        if (token != Token.Empty)
+                        {
+                            hasEmpty = false;
+                            tokens.Add(token);
+                            break;
+                        }
+                    }
+                    else if (item is Rule rule)
+                    {
+                        var ruleFirsts = First(rule.Definitions);
+                        if (!ruleFirsts.Contains(Token.Empty))
+                        {
+                            hasEmpty = false;
+                            tokens.AddRange(ruleFirsts);
+                            break;
+                        }
+                        else
+                        {
+                            tokens.AddRange(ruleFirsts.Where(x => x != Token.Empty));
+                        }
+                    }
+                }
+                if (hasEmpty) tokens.Add(Token.Empty);
+                return tokens;
+            })
+            .Distinct();
+        }
+
+        public IEnumerable<Token> Follow(Rule theRule)
+        {
+            return Rules
+                .SelectMany(knownRule =>
+                {
+                    return knownRule.Definitions
+                        .Where(ruleSequence => ruleSequence.Contains(theRule))
+                        .SelectMany(relatedSequence =>
+                        {
+                            var tokens = new List<Token>();
+                            do
+                            {
+                                relatedSequence = relatedSequence
+                                    .SkipWhile(x => x != theRule)
+                                    .Skip(1);
+
+                                if (relatedSequence.Any())
+                                {
+                                    var firsts = First(new[] { relatedSequence });
+                                    tokens.AddRange(firsts.Where(x => x != Token.Empty));
+                                    if (firsts.Contains(Token.Empty))
+                                    {
+                                        tokens.AddRange(Follow(knownRule));
+                                    }
+                                }
+                                else if (knownRule != theRule)
+                                {
+                                    tokens.AddRange(Follow(knownRule));
+                                }
+                            } while (relatedSequence.Contains(theRule));
+                            return tokens;
+                        });
+                })
+                .DefaultIfEmpty(Token.Eof)
+                .Distinct();
+        }
+
+        public void DumpFF()
+        {
+            foreach (Rule rule in Rules)
+            {
+                Console.Write($"{rule,-15}");
+                Console.Write("{0, -15}", string.Join(",", First(rule.Definitions)));
+                Console.Write("{0, -15}", string.Join(",", Follow(rule)));
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome!\n ***************************************Rao Abdul Raheem*************************************************\n***************************************Saba Saif*************************************************\n***************************************Mah Zaib*************************************************\n***************************************Abdul Basit*************************************************");
+
+
+            var basit = new RuleManager();
+          /*  basit.Rules.Add(basit.CreateTextRule("S->ABC"));
+            basit.Rules.Add(basit.CreateTextRule("A->a/ε"));
+            basit.Rules.Add(basit.CreateTextRule("B->b/ε"));
+            basit.Rules.Add(basit.CreateTextRule("C->c/ε"));
+           
+*/
+
+           
+            basit.Rules.Add(basit.CreateTextRule("S->AbC"));
+           
+            
+            basit.Rules.Add(basit.CreateTextRule("C->c/ε"));
+            basit.Rules.Add(basit.CreateTextRule("A->a/ε"));
+      
+            /* basit.Rules.Add(basit.CreateTextRule("A->a/b/ε"));*/
+
+            /* basit.Rules.Add(basit.CreateTextRule("S->ACB/CbB/Ba"));
+             basit.Rules.Add(basit.CreateTextRule("A->da/BC"));
+             basit.Rules.Add(basit.CreateTextRule("B->g/ε"));
+             basit.Rules.Add(basit.CreateTextRule("C->h/ε"));*/
+
+
+
+
+
+            basit.DumpFF();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********************Abdul Basit*************");
+            int basit = 0;
+            string[] arr;
+            arr = new string[100];
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter the token");
+                arr[i] = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("if You Want to insert Token Then  press 1 and if you want to end program press 0");
+                int b = Convert.ToInt32(Console.ReadLine());
+                if (b == 0)
+                {
+                    goto sa;
+                }
+
+                basit++;
+
+            }
+            sa:
+
+            for (int i = 0; i < basit; i++)
+            {
+                if (arr[i] == "if" || arr[i] == "while" || arr[i] == "break" || arr[i] == "continue" || arr[i] == "double" || arr[i] == "return" || arr[i] == "case" || arr[i] == "sizeof" || arr[i] == "short" || arr[i] == "switch" || arr[i] == "void" || arr[i] == "struct" || arr[i] == "int" || arr[i] == "float" || arr[i] == "char" || arr[i] == "long" || arr[i] == "typedef" || arr[i] == "unsigned" || arr[i] == "static" || arr[i] == "goto" || arr[i] == "string" || arr[i] == "float" || arr[i] == "decimal")
+                {
+                    Console.WriteLine(arr[i] + " is Keyword");
+                }
+                else if (arr[i] == "+" || arr[i] == "-" || arr[i] == "*" || arr[i] == "/" || arr[i] == ">" || arr[i] == "<" || arr[i] == "=")
+                //if (arr[i] == " " || arr[i] == "+" || arr[i] == "-" || arr[i] == "*" || arr[i] == "/" || arr[i] == "," || arr[i] == ";" || arr[i] == ">" || arr[i] == "<" || arr[i] == "=" || arr[i] == "(" || arr[i] == ")" || arr[i] == "[" || arr[i] == "]" || arr[i] == "{" || arr[i] == "}")
+                {
+                    Console.WriteLine(arr[i] + " is OPERATOR");
+                }
+                else if (!arr[i].StartsWith("1") && !arr[i].StartsWith("2") && !arr[i].StartsWith("3") && !arr[i].StartsWith("4") && !arr[i].StartsWith("5") && !arr[i].StartsWith("6") && !arr[i].StartsWith("7") && !arr[i].StartsWith("8") && !arr[i].StartsWith("9") && !arr[i].StartsWith("0"))
+                {
+                    Console.WriteLine(arr[i] + "  IS A VALID IDENTIFIER");
+                }
+
+
+                else if (arr[i].StartsWith("1") || arr[i].StartsWith("2") || arr[i].StartsWith("3") || arr[i].StartsWith("4") || arr[i].StartsWith("5") || arr[i].StartsWith("6") || arr[i].StartsWith("7") || arr[i].StartsWith("8") || arr[i].StartsWith("9") || arr[i].StartsWith("0"))
+                {
+                    if (arr[i].EndsWith("1") || arr[i].EndsWith("2") || arr[i].EndsWith("3") || arr[i].EndsWith("4") || arr[i].EndsWith("5") || arr[i].EndsWith("6") || arr[i].EndsWith("7") || arr[i].EndsWith("8") || arr[i].EndsWith("9") || arr[i].EndsWith("0"))
+                    {
+                        Console.WriteLine(arr[i] + " is CONSTANT");
+                    }
+                    else if (!arr[i].EndsWith("1") || !arr[i].EndsWith("2") || !arr[i].EndsWith("3") || !arr[i].EndsWith("4") || !arr[i].EndsWith("5") || !arr[i].EndsWith("6") || !arr[i].EndsWith("7") || !arr[i].EndsWith("8") || !arr[i].EndsWith("9") || !arr[i].EndsWith("0") || !arr[i].EndsWith("_"))
+                    {
+                        Console.WriteLine(arr[i] + " IS NOT A VALID IDENTIFIER");
+                    }
+
+                }
+            }
+
+        
+  
+        Console.WriteLine("Token is "+basit);
+            Console.ReadLine();
+
+        }
+    }
+}
